7b388bee25e396f594b83626ca0bc5774afd1570
jmatarrita <jmatarrita@Symbiotic.net>
2017-11-20 12:35:44 -0600

    servidores gamma con formulas

--- MiniProyecto 2/MiniProyecto2/src/servidorGamma2.java
+++ MiniProyecto 2/MiniProyecto2/src/servidorGamma2.java
@@ -1,15 +1,17 @@
 
-public class servidorGamma extends servidor {
-	double resultadoGamma=0;
+public class servidorGamma2 extends servidor{
+
+	double resultadoGamma2=0;
 	double lambda=0;
 	
 	public double function(){
 		double r = Math.random()*10;
 		for (int i = 0; i<7; i++) {
 			resultado = Math.log((r-1))/lambda;
-			resultadoGamma = resultadoGamma+resultado;
+			resultadoGamma2 = resultadoGamma2+resultado;
 			r = Math.random()*10;
 		}
-		return resultadoGamma;
+		return resultadoGamma2;
 	}
+
 }
